c5ecc1811230d1c58fa38431e70ccf5f580bd070
934205 <github_mail>
2025-12-17 16:23:52 +0530

    latest

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import Authentication from './components/Authentication.jsx';
+import PaymentButton from './components/PaymentButton.jsx';
 
 function App() {
 
@@ -9,7 +10,8 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<Authentication />} />          
+          <Route path="/" element={<Authentication />} />  
+          <Route path="/pay" element={<PaymentButton />} />          
         </Routes>
       </BrowserRouter>
     </div>
